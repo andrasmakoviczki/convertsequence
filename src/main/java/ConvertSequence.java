084304301071c6459b620ead62d7c8a1f6f143e9
Andras Makoviczki <makoviczki.andras@gmail.com>
2018-04-30 11:37:44 +0200

    Add Compression

--- src/main/java/ConvertSequence.java
+++ src/main/java/ConvertSequence.java
@@ -52,7 +52,7 @@ public class ConvertSequence {
         SequenceFile.Writer writer = null;
 
         try {
-            write(conf, fs, inputPath, outputPath, writer, SequenceFile.CompressionType.RECORD,new GzipCodec());
+            write(conf, fs, inputPath, outputPath, writer, SequenceFile.CompressionType.BLOCK,new GzipCodec());
         } catch (Exception e) {
             e.printStackTrace();
         }
