3fab304fbf4b5342fec865481cc2874b404e3edb
Andras Makoviczki <makoviczki.andras@gmail.com>
2018-04-30 10:43:43 +0200

    set path

--- src/main/java/ConvertSequence.java
+++ src/main/java/ConvertSequence.java
@@ -63,7 +63,7 @@ public class ConvertSequence {
                 writer.append(new Text(status.getPath().toString()), new BytesWritable(buffer));
             }
         } catch (Exception e) {
-            System.out.println("Exception MESSAGES = " + e.getMessage() + " \n" + e.getStackTrace());
+            e.printStackTrace();
         } finally {
             IOUtils.closeStream(writer);
         }
