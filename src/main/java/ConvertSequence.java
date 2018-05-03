f0b78fad0798517d0ac848d01cce01cb13194869
Andras Makoviczki <makoviczki.andras@gmail.com>
2018-05-03 14:06:13 +0200

    Without read

--- src/main/java/ConvertSequence.java
+++ src/main/java/ConvertSequence.java
@@ -48,7 +48,7 @@ public class ConvertSequence {
         SequenceFile.Writer writer = null;
 
         try {
-            write(conf, fs, inputPath, outputPath, writer, SequenceFile.CompressionType.BLOCK,new GzipCodec());
+            write(conf, fs, inputPath, outputPath, writer, SequenceFile.CompressionType.RECORD,new GzipCodec());
         } catch (Exception e) {
             e.printStackTrace();
         }
