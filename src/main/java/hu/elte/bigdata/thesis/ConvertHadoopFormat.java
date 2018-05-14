6bc948be85ee1c9a342bc6fcee6796a89fb17cb2
Andras Makoviczki <makoviczki.andras@gmail.com>
2018-05-14 16:25:38 +0200

    Codeformat

--- src/main/java/hu/elte/bigdata/thesis/ConvertHadoopFormat.java
+++ src/main/java/hu/elte/bigdata/thesis/ConvertHadoopFormat.java
@@ -1,3 +1,5 @@
+package hu.elte.bigdata.thesis;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileStatus;
@@ -12,6 +14,7 @@ import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.compress.BZip2Codec;
 import org.apache.hadoop.io.compress.CompressionCodec;
 
 import org.slf4j.Logger;
@@ -23,84 +26,102 @@ import java.net.URI;
 /**
  * Created by AMakoviczki on 2018. 04. 30..
  */
-public class ConvertSequence {
-    private static final Logger LOG = LoggerFactory.getLogger(ConvertSequence.class);
+public class ConvertHadoopFormat {
+    private static final Logger LOG = LoggerFactory.getLogger(ConvertHadoopFormat.class);
 
     public static void main(String[] args) throws IOException {
 
-        if (args.length < 1) {
-            System.out.println("Usage: convertsequence "
-                    + " <hdfs_path>");
+        if (args.length < 3) {
+            System.out.println("Usage: ConvertHadoopFormat "
+                    + " <input path>" + " <output path>" + " <mode>");
             System.exit(1);
         }
 
-        String hdfsPath = args[0];
-        String seqPath = args[1];
+        //Set arguments
+        String inputarg = args[0];
+        String outputarg = args[1];
+        String mode = args[2];
 
+        //Set configuration
         Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", hdfsPath);
+        conf.set("fs.defaultFS", inputarg);
         conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
         conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
         conf.set("yarn.app.mapreduce.am.staging-dir", "/tmp");
 
-        FileSystem fs = FileSystem.get(URI.create(hdfsPath), conf);
-        Path inputPath = new Path(hdfsPath);
-        Path outputPath = new Path(seqPath);
+        //Create target directory
+        FileSystem fs = FileSystem.get(URI.create(inputarg), conf);
+        Path inputPath = new Path(inputarg);
+        Path outputPath = new Path(outputarg);
 
         if (!fs.exists(inputPath)) {
             System.out.println("Path " + inputPath + " does not exists");
             System.exit(1);
         }
 
-        writeToHbase(conf, fs, inputPath, seqPath);
-
-        /*SequenceFile.Writer writer = null;
-
-        try {
-            write(conf, fs, inputPath, outputPath, writer, null, null);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
-        /*MapFile.Writer writer = null;
-        try {
-            writeMap(conf, fs, inputPath, outputPath, writer, SequenceFile.CompressionType.BLOCK,new GzipCodec());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
-        /*SequenceFile.Reader reader = null;
-
-        try {
-            read(conf,fs,outputPath,reader);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
-        /*MapFile.Reader reader = null;
-
-        try {
-            readMap(conf,fs,outputPath,reader);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+        if (mode.equals("hbase")) {
+            SequenceFile.Writer writer = null;
+            writeToHbase(conf, fs, inputPath, outputarg);
+        } else if (mode.equals("seqnc")) {
+            try {
+                SequenceFile.Writer writer = null;
+                write(conf, fs, inputPath, outputPath, writer, null, null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (mode.equals("seqrc")) {
+            SequenceFile.Writer writer = null;
+            try {
+                write(conf, fs, inputPath, outputPath, writer, SequenceFile.CompressionType.RECORD, new BZip2Codec());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (mode.equals("seqbc")) {
+            SequenceFile.Writer writer = null;
+            try {
+                write(conf, fs, inputPath, outputPath, writer, SequenceFile.CompressionType.BLOCK, new BZip2Codec());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (mode.equals("map")) {
+            MapFile.Writer writer = null;
+            try {
+                writeMap(conf, fs, inputPath, outputPath, writer, SequenceFile.CompressionType.BLOCK, new BZip2Codec());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (mode.equals("readseq")) {
+            SequenceFile.Reader reader = null;
+            try {
+                readSeq(conf, fs, outputPath, reader);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (mode.equals("readmap")) {
+            MapFile.Reader reader = null;
+            try {
+                readMap(conf, fs, outputPath, reader);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void write(Configuration conf, FileSystem fs, Path inputPath, Path outputPath, SequenceFile.Writer writer, SequenceFile.CompressionType cType, CompressionCodec cCodec) throws IOException {
         FileStatus[] fileStatuses = fs.listStatus(inputPath);
+
+        //Initialize writer
         writer = SequenceFile.createWriter(conf,
                 SequenceFile.Writer.file(outputPath),
                 SequenceFile.Writer.keyClass(Text.class),
                 SequenceFile.Writer.valueClass(BytesWritable.class));
 
+        //Write input into SequenceFile
         for (FileStatus status : fileStatuses) {
             FSDataInputStream file = fs.open(status.getPath());
-
             byte buffer[] = new byte[file.available()];
             file.read(buffer);
-
             writer.append(new Text(status.getPath().toString()), new BytesWritable(buffer));
-
             file.close();
         }
         IOUtils.closeStream(writer);
@@ -108,23 +129,30 @@ public class ConvertSequence {
 
     public static void writeMap(Configuration conf, FileSystem fs, Path inputPath, Path outputPath, MapFile.Writer writer, SequenceFile.CompressionType cType, CompressionCodec cCodec) throws IOException {
         FileStatus[] fileStatuses = fs.listStatus(inputPath);
-        writer = new MapFile.Writer(conf,
-                outputPath,
-                MapFile.Writer.keyClass(Text.class),
-                MapFile.Writer.valueClass(BytesWritable.class),
-                SequenceFile.Writer.valueClass(BytesWritable.class),
-                SequenceFile.Writer.compression(cType, cCodec));
 
+        //Initialize writer
+        if (!cType.equals(null)) {
+            writer = new MapFile.Writer(conf,
+                    outputPath,
+                    MapFile.Writer.keyClass(Text.class),
+                    MapFile.Writer.valueClass(BytesWritable.class),
+                    SequenceFile.Writer.valueClass(BytesWritable.class),
+                    SequenceFile.Writer.compression(cType, cCodec));
+        } else {
+            writer = new MapFile.Writer(conf,
+                    outputPath,
+                    MapFile.Writer.keyClass(Text.class),
+                    MapFile.Writer.valueClass(BytesWritable.class),
+                    SequenceFile.Writer.valueClass(BytesWritable.class));
+        }
+
+
+        //Write input into MapFile
         for (FileStatus status : fileStatuses) {
-            System.out.println(status.getPath().toString());
-
             FSDataInputStream file = fs.open(status.getPath());
-
             byte buffer[] = new byte[file.available()];
             file.read(buffer);
-
             writer.append(new Text(status.getPath().toString()), new BytesWritable(buffer));
-
             file.close();
         }
         IOUtils.closeStream(writer);
@@ -133,15 +161,17 @@ public class ConvertSequence {
     public static void writeToHbase(Configuration conf, FileSystem fs, Path inputPath, String tname) throws IOException {
         FileStatus[] fileStatuses = fs.listStatus(inputPath);
 
+        //HBase configuration
         Configuration hconf = HBaseConfiguration.create();
         hconf.addResource(new Path("/etc/hbase/conf/hbase-site.xml"));
         hconf.addResource(new Path("/etc/hadoop/conf/core-site.xml"));
 
+        //Set connecton
         Connection connection = ConnectionFactory.createConnection(hconf);
         Admin admin = connection.getAdmin();
 
+        //Create HBase table if not exists
         TableName tableName = TableName.valueOf(tname);
-
         if (!admin.tableExists(tableName)) {
             HTableDescriptor hbaseTable = new HTableDescriptor(tableName);
             HColumnDescriptor hbaseColumn = new HColumnDescriptor("image");
@@ -154,6 +184,7 @@ public class ConvertSequence {
 
         Table hTable = connection.getTable(tableName);
 
+        //Insert data into table
         for (FileStatus status : fileStatuses) {
             FSDataInputStream file = fs.open(status.getPath());
             byte buffer[] = new byte[file.available()];
@@ -167,30 +198,32 @@ public class ConvertSequence {
         }
     }
 
-    public static void read(Configuration conf, FileSystem fs, Path outputPath, SequenceFile.Reader reader) throws IOException {
+    public static void readSeq(Configuration conf, FileSystem fs, Path outputPath, SequenceFile.Reader reader) throws IOException {
+        //Open file
         reader = new SequenceFile.Reader(conf,
                 SequenceFile.Reader.file(outputPath));
 
         Text key = new Text();
         BytesWritable value = new BytesWritable();
 
+        //Read content
         while (reader.next(key, value)) {
             System.out.println(key.toString() + " " + value.getLength());
         }
-
         IOUtils.closeStream(reader);
     }
 
     public static void readMap(Configuration conf, FileSystem fs, Path outputPath, MapFile.Reader reader) throws IOException {
+        //Open file
         reader = new MapFile.Reader(outputPath, conf);
 
         Text key = new Text();
         BytesWritable value = new BytesWritable();
 
+        //Read content
         while (reader.next(key, value)) {
             System.out.println(key.toString() + " " + value.getLength());
         }
-
         IOUtils.closeStream(reader);
     }
 
